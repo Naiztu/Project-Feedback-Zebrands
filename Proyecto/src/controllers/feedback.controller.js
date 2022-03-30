3d5d6ceb8e4899a91a8f473e8986f853e5f197cf
LeilaelRico <A01707023@tec.mx>
2022-03-30 15:16:23 -0600

    Se integra función de POST para registrar Feedback.

--- Proyecto/src/controllers/feedback.controller.js
+++ Proyecto/src/controllers/feedback.controller.js
@@ -17,4 +17,57 @@ async function getFeedback(req, res) {
       res.status(500).send({ err });
     });
 }
-module.exports = { getFeedback };
+
+
+async function postFeedback(req, res) {
+  const id_assistant = req.params.id_assistant;
+  const id_member = req.params.id_member;
+  console.log(id_assistant);
+  console.log(id_member);
+
+
+  pool
+    .execute(
+
+      `INSERT INTO feedback (
+        calificacion_craft,
+        calificacion_personal,
+        calificacion_business,
+        calificacion_promedio,
+        comentario_craft,
+        comentario_personal,
+        comentario_business,
+        comentario_general,
+        id_empleado_member,
+        id_empleado_assistant,
+        id_periodo
+        )
+        VALUES (
+            ${req.body.calificacion_craft},
+            ${req.body.calificacion_personal},
+            ${req.body.calificacion_business},
+            ${req.body.calificacion_promedio},
+            ${req.body.comentario_craft},
+            ${req.body.comentario_personal},
+            ${req.body.comentario_business},
+            ${req.body.comentario_general},
+            ${id_member},
+            ${id_assistant},
+            ${req.body.id_periodo}        
+        )`
+
+    )
+    .then(() => {
+      console.log ("Si jala");
+      res.status(200).end();
+  
+    })
+    .catch((err) => {
+      res.status(500).send({ err });
+    });
+}
+
+module.exports = { getFeedback, postFeedback };
+
+
+
