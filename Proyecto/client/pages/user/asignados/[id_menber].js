22dc5c55c9f6caabc8f1cca50834417056082edf
Naiztu <A01707404@tec.mx>
2022-03-30 12:55:30 -0600

    fixed deploy

--- Proyecto/client/pages/user/asignados/[id_menber].js
+++ Proyecto/client/pages/user/asignados/[id_menber].js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../../../components/Layout";
 import PlantillaFeed from "../../../components/PlantillaFeed";
 
-export default function () {
+export default function Post() {
   return (
     <Layout>
       <div className="pt-20"> Aqui van a ir las evaluaciones</div>
