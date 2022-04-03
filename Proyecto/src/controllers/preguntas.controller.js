e4bd63faff122f6e1f179c5aabc3e8dce82cd334
LeilaelRico <A01707023@tec.mx>
2022-04-02 19:16:09 -0600

    Se añade aproximación a cambio de index.

--- Proyecto/src/controllers/preguntas.controller.js
+++ Proyecto/src/controllers/preguntas.controller.js
@@ -30,13 +30,12 @@ async function registraPregunta(req, res) {
     nivel_pregunta,
     dimension_pregunta,
     tipo_pregunta,
-    id_chapter
-  } = req.body
+    id_chapter,
+  } = req.body;
 
-
-pool
-  .execute(
-    `
+  pool
+    .execute(
+      `
     INSERT INTO banco_preguntas (
       pregunta,
       index_pregunta,
@@ -48,44 +47,66 @@ pool
       VALUES (
          '${pregunta}',
          (SELECT MAX(b2.index_pregunta)+1 
-  FROM banco_preguntas b2
-  WHERE b2.id_chapter=${id_chapter} AND b2.nivel_pregunta=${nivel_pregunta} AND b2.dimension_pregunta='${dimension_pregunta}'),
+          FROM banco_preguntas b2
+          WHERE b2.id_chapter=${id_chapter} AND b2.nivel_pregunta=${nivel_pregunta} AND b2.dimension_pregunta='${dimension_pregunta}'),
          ${nivel_pregunta},
          '${dimension_pregunta}',
          '${tipo_pregunta}',
          ${id_chapter}
   )`
-  )
-  .then(() => {
-    console.log ("Si jala registrar");
-    res.status(200).end();
-
-  })
-  .catch((err) => {
-    res.status(500);
-    res.send({ err });
-  });
+    )
+    .then(() => {
+      res.status(200).send({Message: "Si jala el post de registrar"}).end();
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send({ err });
+    });
 }
-
 
 async function eliminaPregunta(req, res) {
-  
   const id_pregunta = req.params.id_pregunta;
 
-
-pool
-  .execute(
-    `DELETE FROM banco_preguntas WHERE id_pregunta=${id_pregunta};`
-  )
-  .then(() => {
-    console.log ("Si jala eliminar");
-    res.status(200).end();
-  })
-  .catch((err) => {
-    res.status(500);
-    res.send({ err });
-  });
+  pool
+    .execute(`DELETE FROM banco_preguntas WHERE id_pregunta=${id_pregunta};`)
+    .then(() => {
+      console.log("Si jala eliminar");
+      res.status(200).end();
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send({ err });
+    });
 }
 
 
-module.exports = { getPreguntas ,registraPregunta, eliminaPregunta };
+async function cambioLugar(req, res) {
+
+  const {
+
+    id_pregunta_origen,
+    id_pregunta_destino,
+    index_pregunta_destino
+
+  } = req.body;
+
+  pool
+    .execute(
+      `
+        start transaction;    
+        set @myval = (select index_pregunta from banco_preguntas where id_pregunta = ${id_pregunta_origen});
+        update banco_preguntas set index_pregunta = @myval where id_pregunta = ${id_pregunta_destino};
+        update banco_preguntas set index_pregunta = ${index_pregunta_destino} where id_pregunta = ${id_pregunta_origen};
+        commit;
+      `
+    )
+    .then(() => {
+      res.status(200).send({Message: "Si jala el post de cambiar de lugar"}).end();
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send({ err });
+    });
+}
+
+module.exports = { getPreguntas, registraPregunta, eliminaPregunta, cambioLugar };
--- Proyecto/src/routes/preguntas.routes.js
+++ Proyecto/src/routes/preguntas.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 
-const { getPreguntas, registraPregunta, eliminaPregunta } = require("../controllers/preguntas.controller");
+const { getPreguntas, registraPregunta, eliminaPregunta, cambioLugar } = require("../controllers/preguntas.controller");
 
 
 const router = Router();
@@ -10,6 +10,8 @@ router.get("/:nivel/:dimension", getPreguntas);
 router.post("/registra", registraPregunta);
 
 router.delete("/:id_pregunta", eliminaPregunta);
+
+router.post("/cambio", cambioLugar);
 
 
 module.exports = router;
