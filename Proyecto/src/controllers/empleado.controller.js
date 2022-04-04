bafcb9c4dfcc9691ea1c5995eb7ff6194318677e
AudibleParrot2 <A01703259@tec.mx>
2022-04-04 16:01:50 -0500

    ?

--- Proyecto/src/controllers/empleado.controller.js
+++ Proyecto/src/controllers/empleado.controller.js
@@ -4,10 +4,8 @@ export async function getEmpleado(req, res) {
   const { id_empleado } = req.params;
   const empleado = new Empleado (id_empleado);
   try {
-    const [rows, fields] = await pool.execute(
-      `SELECT id_empleado, nombre, apellido_paterno, nivel_craft, nivel_people, nivel_business, imagen_perfil 
-      FROM empleado WHERE id_empleado = ${id_empleado}`
-    );
+    const data_empleado = await empleado.getDataEmpleado();
+    res.send({data_empleado});
     // rows.length === 0
     //   ? res.status(403).send({ err: "No hay ese empleado" })
     //   : res.status(200).send({ empleado: rows[0] });
