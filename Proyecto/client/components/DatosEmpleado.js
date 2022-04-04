65dfb31ddc81eedc65e4dc7d3f0782d5d3d0154d
Adrian <A01703889@itesm.mx>
2022-04-04 10:58:51 -0500

    Comp InfoEmpleado

--- Proyecto/client/components/DatosEmpleado.js
+++ Proyecto/client/components/DatosEmpleado.js
@@ -1,19 +1,39 @@
 import React from 'react'
+import { FaTrashAlt, FaPencilAlt, FaSave } from "react-icons/fa";
+import swal from "sweetalert";
 
-export default function DatosEmpleado({label, value}) {
+export default function DatosEmpleado({name, value, isSaved}) {
   
 
+  const handleEdit = () => {
+    setIsEdited(true);
+  };
 
   return (
     <>
-    
-    <div>DatosEmpleado</div>
-    <div className="flex space-x-5 flex-row w-full mx-auto mt-9 px-6 py-4 bg-slate-500/10 rounded-lg">
-    <label>
-      {label}
+    <table className=" mx-auto mb-5  bg-white shadow-lg rounded-sm border border-gray-200 w-11/12 sm:w-3/4">
+      <div className="flex space-x-20 flex-row w-full mx-auto mt-9 px-6 py-4 bg-slate-500/10 rounded-lg">
+      <label className=" text-2xl font-bold mb-2 ml-6">
+      {name}
     </label>
-    <p> {value} </p>
+    <tr className=" text-2xl font-bold mb-2 ml-6">
+      {value} </tr>
+      
+
+      <div className=" flex flex-col space-y-3">
+          <button
+            className="btn"
+          >
+          <FaSave />  
+          </button>
+          <button
+            className={`btn-red "hidden" : "flex"`}
+          >
+            <FaPencilAlt />
+          </button>
+        </div>
     </div>
+    </table>
     </>
   )
 }
--- Proyecto/client/pages/prueba.js
+++ Proyecto/client/pages/prueba.js
@@ -6,9 +6,26 @@ import DatosEmpleado from "../components/DatosEmpleado";
 export default function Prueba() {
   return (
     <>
+          <header className=" bg-slate-400/10 w-full pt-10 rounded-b-3xl">
+        <div className="flex flex-col justify-center items-center w-10/12 mx-auto">
+          <h1  className=" title my-5 mb-7 mt-3">
+            Mi perfil
+          </h1>
+        </div>
+      </header>
 
-      <DatosEmpleado label={"nombre"} value={"Felipe Neduro"}/>
-      <DatosEmpleado label={5} value={5}/>
+      <DatosEmpleado name={"nombre"} value={"Felipe"}/>
+      <DatosEmpleado name={"Apellido paterno"} value={"Neduro"}/>
+      <DatosEmpleado name={"Apellido materno"} value={"Garcia"}/>
+      
+      <DatosEmpleado name={"Correo"} value={"noobmaster69@gmail.com"}/>
+      <DatosEmpleado name={"Contraseña"} value={"*******"}/>
+      <DatosEmpleado name={"Rol del usuario"} value={"Chapter Lead Assistant"}/>
+      <DatosEmpleado name={"Chapter"} value={"Software"}/>
+      <DatosEmpleado name={"Equipo"} value={"Zecore Client"}/>
+      <DatosEmpleado name={"Chapter Lead"} value={"Edgar Santanta Matute"}/>
+
+
 
     </>
   );
