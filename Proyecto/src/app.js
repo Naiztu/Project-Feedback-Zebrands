106501bb949ae7884f6152e47292542396a326c4
FerminMendez <A01703366@tec.mx>
2022-04-05 09:46:25 -0500

    borra example

--- Proyecto/src/app.js
+++ Proyecto/src/app.js
@@ -6,7 +6,6 @@ import cors from "cors";
 //importing routes
 import rutas_feedback from "./routes/feedback.routes";
 import rutas_preguntas from "./routes/preguntas.routes";
-import rutas_example from "./routes/example.routes"; //Para probar cosas
 import rutas_assistant_list from "./routes/assistant_list.routes"; //Para probar cosas
 import rutas_evaluar from "./routes/evaluar.routes";
 import rutas_empleado from "./routes/empleado.routes";
@@ -23,7 +22,6 @@ app.use(express.static("public"));
 //Routes
 app.use("/feedback", rutas_feedback);
 app.use("/preguntas", rutas_preguntas);
-app.use("/example", rutas_example);
 app.use("/assistant_list", rutas_assistant_list);
 app.use("/evaluar", rutas_evaluar);
 app.use("/empleado", rutas_empleado);
