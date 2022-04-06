20e9c7eebf57bad5c1cddd72d3f534481793d12d
FerminMendez <A01703366@tec.mx>
2022-04-06 16:48:39 -0500

    transaction con registrar preguntas y update estatus

--- Proyecto/src/models/respuestas.model.js
+++ Proyecto/src/models/respuestas.model.js
@@ -2,9 +2,9 @@ import pool from "../database/db";
 import { queryPostRespuestas } from "../util/query";
 
 
-export class Respuesta{
+export class Respuesta {
 
-  static async getRes(id_evaluado, id_evaluador, id_periodo ) {
+  static async getRes(id_evaluado, id_evaluador, id_periodo) {
     try {
       const [rows, fields] = await pool.execute(
         `SELECT * FROM respuesta
@@ -18,48 +18,61 @@ export class Respuesta{
       throw { err };
     }
   }
-    
+
 
 }
 
 
-export class pstRespuesta{
+export class pstRespuesta {
 
-    constructor( 
+  constructor(
 
-        _id_empleado_evaluador,
-        _id_empleado_evaluado,
-        _id_periodo,
-        _lista_preguntas
+    _id_empleado_evaluador,
+    _id_empleado_evaluado,
+    _id_periodo,
+    _lista_preguntas
 
-        )
-        {
-       
-            this.id_empleado_evaluador = _id_empleado_evaluador;
-            this.id_empleado_evaluado = _id_empleado_evaluado;
-            this.id_periodo = _id_periodo;
-            this.lista_preguntas = _lista_preguntas;
+  ) {
 
+    this.id_empleado_evaluador = _id_empleado_evaluador;
+    this.id_empleado_evaluado = _id_empleado_evaluado;
+    this.id_periodo = _id_periodo;
+    this.lista_preguntas = _lista_preguntas;
+
+  }
+
+  async postRespuestas() {
+
+        let conn = null;
+        try {
+        conn = await pool.getConnection();
+        await conn.beginTransaction();
+  
+
+        const [rows, fields] = await conn.query(`
+        ${queryPostRespuestas(this.id_empleado_evaluador,
+        this.id_empleado_evaluado,
+        this.id_periodo,
+        this.lista_preguntas)}
+        `);
+        
+        await conn.query(`
+        UPDATE evaluacion SET estatus = 'Contestado' WHERE evaluacion.id_empleado_evaluador = ${this.id_empleado_evaluador} 
+        AND evaluacion.id_empleado_evaluado = ${this.id_empleado_evaluado} AND evaluacion.id_periodo = ${this.id_periodo}
+        `);
+        
+    
+        
+        await conn.commit();
+        await conn.release();
+      } catch (error) {
+        if (conn) {
+            await conn.rollback();
+            await conn.release();
         }
-
-        async postRespuestas() {
-            try {
-              const [rows, fields] = await pool.execute(
+        throw error;
+      }
 
 
-                `
-                ${queryPostRespuestas(this.id_empleado_evaluador,
-                    this.id_empleado_evaluado,
-                    this.id_periodo,
-                    this.lista_preguntas )   
-                }
-                `
-              );
-              return rows;
-            } catch (err) {
-                console.log(err)
-              throw { err };
-            }
-          }
-
+    }
 }
